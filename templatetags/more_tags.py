9d62ef192fdb3b499d0ab318b300cadedbd06247
Harsh Kankrecha <108125806+harshkankrecha@users.noreply.github.com>
2023-07-24 10:39:56 +0530

    Delete templatetags directory

--- templatetags/more_tags.py
+++
@@ -1,8 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter(name='has_user_solved')
-def has_user_solved(problem, user_obj):
-    return problem.users.filter(id=user_obj.id).exists()
-
